79705ab18beab213a32634be1bd9fcf4bb05466d
Fernando (trend) <fom78a@gmail.com>
2024-07-18 07:46:59 -0300

    add thiago cv

---
+++ cvs/cv.js
@@ -0,0 +1,8 @@
+
+import cv from "../cvs/cv_fom_dev_EN.json"
+import cv1 from "../cvs/cv_fom_industrial.json"
+import cvThiago from "../cvs/cv_thiago.json"
+import cvYose from "../cvs/cv_yoselin.json"
+
+const {basics,work,skills,language_to_show,languages,education,projects} = cvThiago
+export {basics,work,skills,language_to_show,languages,education,projects}
